6b7d29b5f2528d06442edfc388e8fcbd195c02ea
Rahul <rahul@mindpathtech.com>
2021-11-28 13:10:58 +0530

    updated code

--- actions/actions.py
+++ actions/actions.py
@@ -1,10 +1,10 @@
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
+from rasa_sdk.events import AllSlotsReset
 from rasa_sdk.executor import CollectingDispatcher
 from helpers.requestHelper import RequestUtility
 from configs.config import API_URL
 from helpers.dateConvertor import dateTimeConvertor
-
 
 
 class ActionGetCovidDetails(Action):
@@ -21,29 +21,43 @@ class ActionGetCovidDetails(Action):
             "GET", API_URL)
         response = (response.json())
         status = tracker.get_slot("status")
-        state = tracker.get_slot("state")
+        states = tracker.get_slot("state")
         date = tracker.get_slot("date")
+        flag = False
+        caseStatusMessage = "Covid cases status: "
 
         if date:
             formatedDate = dateTimeConvertor(date)
             data = response.get("data")
-            for values in data:
-                if values.get("day") == formatedDate:
-                    covidCaseStatus=values.get("summary").get(status)
-                    if state:
-                        for item in state:
-                            for regionalData in values.get("regional"):
-                                if regionalData.get("loc").lower() == item.lower():
-                                    covidCaseStatus=regionalData.get(status)
-                                    dispatcher.utter_message(text = f"Covid case on {formatedDate} in {item} is {covidCaseStatus}")
-                                    return []
-                    dispatcher.utter_message(text = f"Covid case status on {formatedDate} is {covidCaseStatus}")
+            try:
+                for values in data:
+                    if values.get("day") == formatedDate:
+                        flag = True
+                        covidCaseStatus = values.get("summary").get(status)
+                        if states:
+                            for state in states:
+                                for regionalData in values.get("regional"):
+                                    if regionalData.get("loc").lower() == state.lower():
+                                        covidCaseStatus = regionalData.get(
+                                            status)
+                                        caseStatusMessage += f"\n{date[0]}\n{state} : {covidCaseStatus} "
+                            dispatcher.utter_message(text=caseStatusMessage)
+                            return [AllSlotsReset()]
+                        break
+                if flag == False:
+                    dispatcher.utter_message(
+                        text=f"Sorry, We don't have any data for {date[0]}")
 
-        elif state:
-            for value in state:
+            except:
+                dispatcher.utter_message(
+                    text="We are having some issue now please try after some time")
+
+        elif states:
+            for state in states:
                 for regionalData in response.get("data")[0].get("regional"):
-                    if regionalData.get("loc").lower() == value.lower():
-                        covidCaseStatus=regionalData.get(status)
-                        dispatcher.utter_message(text = f"Covid case status in {value} is {covidCaseStatus}")
-            
-        return []
+                    if regionalData.get("loc").lower() == state.lower():
+                        covidCaseStatus = regionalData.get(status)
+                        caseStatusMessage += f"\n{state} : {covidCaseStatus} "
+            dispatcher.utter_message(text=caseStatusMessage)
+
+        return [AllSlotsReset()]
--- helpers/dateConvertor.py
+++ helpers/dateConvertor.py
@@ -1,7 +1,11 @@
 import datetime
 
-def dateTimeConvertor(date):
-    for value in date:
-        formatedDate = datetime.datetime.strptime(value, "%dst %b %Y" or "%d %b %Y" or "%dnd %b %Y" or "%dth %b %Y" or "%d, %b %Y" or"%Y/%m/%d" or "%d-%m-%Y" or "%Y%m%d")
-        formatedDate = formatedDate.strftime('%Y-%m-%d')
-    return formatedDate
+
+def dateTimeConvertor(dates):
+    for date in dates:
+        for fmt in ["%Y/%m/%d", "%d-%m-%Y", "%Y%m%d", "%dst %b %Y", "%d %b %Y", "%dnd %b %Y", "%dth %b %Y", "%d, %b %Y"]:
+            try:
+                return str(datetime.datetime.strptime(date, fmt).date())
+            except ValueError:
+                continue
+        raise ValueError(date)
